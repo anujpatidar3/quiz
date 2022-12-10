6aa1403032e85625737935d31bf06a2030767c98
anujpatidar3 <anujpatidar3@gmail.com>
2022-12-10 20:24:55 +0530

    Routing completed

--- src/App.js
+++ src/App.js
@@ -3,16 +3,23 @@ import { BrowserRouter, Route, Routes } from 'react-router-dom';
 import Questions from './pages/Questions';
 import Settings from './pages/Settings';
 import FinalScreen from './pages/FinalScreen';
+import { Container, Typography } from '@mui/material';
+import { Box } from '@mui/system';
 
 function App() {
   return (
     <div className="App">
       <BrowserRouter>
-        <Routes>
-          <Route path='/questions' element={<Questions />} />
-          <Route path='/' element={<Settings />} />
-          <Route path='/score' element={<FinalScreen />} />
-        </Routes>
+        <Container maxWidth="sm">
+          <Box textAlign="center" mt={5}>
+            <Typography variant="h2" fontWeight="bold"> Quiz App</Typography>
+            <Routes>
+              <Route path='/questions' element={<Questions />} />
+              <Route path='/' element={<Settings />} />
+              <Route path='/score' element={<FinalScreen />} />
+            </Routes>
+          </Box>
+        </Container>
       </BrowserRouter>
     </div>
   );
